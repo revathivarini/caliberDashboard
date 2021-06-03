f8895bfbc5c23f603be459384c4bfcb628f823a9
Revathi <reva.avula@gmail.com>
2021-06-03 17:54:07 -0500

    Caliber Project

---
+++ dashboard/src/main/java/org/caliber/dashboard/CaliberAppApplication.java
@@ -0,0 +1,13 @@
+package org.caliber.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliberAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliberAppApplication.class, args);
+	}
+
+}
---
+++ dashboard/src/test/java/org/caliber/dashboard/CaliberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.caliber.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
